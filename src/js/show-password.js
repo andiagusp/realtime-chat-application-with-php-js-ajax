fae1f7d473533ca0d16b67bf6be9cfaeae7e0178
andiagusp <aap07081998@gmail.com>
2021-03-06 19:10:40 +0700

    html css styling

---
+++ src/js/show-password.js
@@ -0,0 +1,6 @@
+const passwordField = document.querySelector(".form .field input[type=password]");
+const eyeButton = document.querySelector(".form .field i");
+
+eyeButton.addEventListener('click', function() {
+   alert('oke') 
+});
